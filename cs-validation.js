a4b50375f77d95b4567d3b0f9ad2c55b0951f608
Cody Sirk <csirk005@gmail.com>
2017-04-21 23:53:07 -0500

    adding methods

--- cs-validation.js
+++ cs-validation.js
@@ -4,4 +4,12 @@ function validateEqual(item1, item2) {
     }
 }
 
-function 
+function verifyStateChanged(elements, originalValues, elementToEnable) {
+    // Get elements by jquery id
+
+    // Get current state of objects
+
+    // Get original state of objects from list
+
+    // If new state is not equal to old state then enable
+}
